6e0ec80c72c9e744261eee8e151f4d17bcf076d0
kuper0201 <kuper171@gmail.com>
2023-09-16 02:42:32 +0900

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -6,13 +6,11 @@ chmod +x autovnc.sh
 
 # NVIDIA 드라이버
 apt update
-apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-apt install nvidia-driver-535
+apt -y install openbox sddm xterm firefox
+apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common nvidia-driver-535
 
 # Docker 설치
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
-apt install docker-ce
-
-apt install openbox sddm xterm firefox
+apt -y install docker-ce
