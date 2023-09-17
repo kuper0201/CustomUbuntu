a91036d3672f7aea643cdbff7b04e1a766ed297b
kuper0201 <kuper171@gmail.com>
2023-09-17 13:40:32 +0900

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -8,9 +8,11 @@ chmod +x autovnc.sh
 apt update
 apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common nvidia-driver-535
 
-# 키 추가
+# Container Toolkit 키 추가
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+# Docker 키 추가
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
